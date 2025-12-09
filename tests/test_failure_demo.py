742e5ae6f45cd7f890bba01f6feab91c339294db
MAnitsu <darrek96@gmail.com>
2025-12-09 17:14:17 +0200

    [Improvements] Deleted unnecessary step from test

--- tests/test_failure_demo.py
+++ tests/test_failure_demo.py
@@ -6,5 +6,4 @@ import allure
 @allure.severity(allure.severity_level.CRITICAL)
 def test_intentional_failure(page):
     page.goto("https://example.com")
-    allure.attach(page.screenshot(), name="before_assert", attachment_type=allure.attachment_type.PNG)
     assert page.locator("h1").inner_text() == "Non-Existent Text"
